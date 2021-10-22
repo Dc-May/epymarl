780a55569cfc8fe0615f44a9fc1d1c4382943bcb
Peter <atrazhev@ualberta.ca>
2021-10-22 00:34:35 -0600

    configs for lbf hyperparameters
    start of gym runner

--- src/runners/__init__.py
+++ src/runners/__init__.py
@@ -5,3 +5,6 @@ REGISTRY["episode"] = EpisodeRunner
 
 from .parallel_runner import ParallelRunner
 REGISTRY["parallel"] = ParallelRunner
+
+from .gym_runner import GymRunner
+REGISTRY['gym'] = GymRunner
---
+++ src/runners/gym_runner.py
@@ -0,0 +1,44 @@
+from envs import REGISTRY as env_REGISTRY
+import numpy as np 
+from functools import partial
+from components.episode_buffer import EpisodeBatch
+
+class GymRunner:
+    def __init__(self, args, logger):
+        self.args = args
+        self.logger = logger
+        self.batch_size = self.args.batch_size_run 
+        assert self.batch_size == 1 # make sure batches are one
+        self.env = env_REGISTRY[self.args.env](**self.args.env_args)
+        self.episode_limit = self.env.episode_limit
+        self.t = 0
+
+        self.t_env = 0 
+        # Log the first run
+        self.log_train_stats_t = -1000000
+    
+    def setup(self, scheme, groups, preprocess, mac):
+        self.new_batch = partial(EpisodeBatch, scheme, groups, self.batch_size, self.episode_limit + 1,
+                                 preprocess=preprocess, device=self.args.device)
+        self.mac = mac
+
+    def get_env_info(self):
+        return self.env.get_env_info()
+
+    def save_replay(self):
+        self.env.save_replay()
+
+    def close_env(self):
+        self.env.close()
+
+    def reset(self):
+        self.batch = self.new_batch()
+        self.env.reset()
+        self.t = 0
+        
+    def run(self, test_mode=False):
+        pass 
+
+    def _log(self, returns, stats, prefix):
+        pass 
+
